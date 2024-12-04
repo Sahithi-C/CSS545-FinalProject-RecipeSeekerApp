9c6f02c405b8051945b7d60ecfcf41985bc017e8
Phani-Sahithi <phani-sahithi@Phani-Sahithis-MacBook-Air.local>
2024-12-03 22:48:15 -0800

    Adding landscape for Favourites page

--- app/src/main/java/com/example/vegrecipeseeker/Favourites.java
+++ app/src/main/java/com/example/vegrecipeseeker/Favourites.java
@@ -1,6 +1,7 @@
 package com.example.vegrecipeseeker;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -21,7 +22,12 @@ public class Favourites extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_favourites);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.activity_favourites_land);
+        } else {
+            setContentView(R.layout.activity_favourites);
+        }
 
         // Setup toolbar
         Toolbar toolbar = findViewById(R.id.favourites_toolbar);
